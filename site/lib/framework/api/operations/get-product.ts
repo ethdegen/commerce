de162318b17c3bc4b2a00e0e6b59b6bac68500e5
ethdegen <eth@degen>
2023-03-30 20:31:59 -0700

    Remove Monorepo Provider Structure

---
+++ site/lib/framework/api/operations/get-product.ts
@@ -0,0 +1,24 @@
+import type { LocalConfig } from '../index'
+import { Product } from '@commerce/types/product'
+import { GetProductOperation } from '@commerce/types/product'
+import data from '../../data.json'
+import type { OperationContext } from '@commerce/api/operations'
+
+export default function getProductOperation(_p: OperationContext<any>) {
+  async function getProduct<T extends GetProductOperation>({
+    query = '',
+    variables,
+    config,
+  }: {
+    query?: string
+    variables?: T['variables']
+    config?: Partial<LocalConfig>
+    preview?: boolean
+  } = {}): Promise<Product | {} | any> {
+    return {
+      product: data.products.find(({ slug }) => slug === variables!.slug),
+    }
+  }
+
+  return getProduct
+}
